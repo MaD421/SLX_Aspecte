5a0e87bd0e1d6bfad9e6dd67f9fc056ea7d1952d
Andrei <andrei.marinoiu@protonmail.com>
2021-03-21 16:50:12 +0200

    removed unused exception models

--- SLX/src/main/java/com/project/SLX/model/exception/BookmarkFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.project.SLX.model.exception;
-
-public class BookmarkFoundException extends RuntimeException {
-
-    public BookmarkFoundException() {
-        super("Listing is already bookmarked!");
-    }
-
-}
